402864ad0b14475a1c448a21638ccc6651063329
ORLANDO VIEIRA NETO <110175293+vieiranetoorlando@users.noreply.github.com>
2024-02-23 22:41:00 +0000

    Endpoint 'GET] /item/:id' Criado

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ app.get('/oi', function (req, res) {
 
   // Read all -> [GET] / item
   app.get('/item', function (req, res){
+    //envio a lista inteiro como resposta HTTP
     res.send(lista)
+  })
+
+  // Read By ID -> [GET] /item/:id
+  app.get('/item/:id', function(req, res){
+    // Acessp o ID no parâ,etro de rota
+    const id = req.params.id
+
+    // Acesso item na lista baeado no ID recebido
+    const item = lista[id]
+
+    // Envio o item obtido como resposta HTTP
+    res.send(item)
   })
 
 app.listen(3000)
