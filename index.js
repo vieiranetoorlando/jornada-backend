d1a91ec079d58318d5910ef0038de8d12f1a579c
ORLANDO VIEIRA NETO <110175293+vieiranetoorlando@users.noreply.github.com>
2024-02-23 21:10:09 +0000

    Endpoint'[GET] / item' criado

--- index.js
+++ index.js
@@ -8,5 +8,12 @@ app.get('/', function (req, res) {
 app.get('/oi', function (req, res) {
     res.send('Olá mundo')
   })
+//lista de personagens
+  const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+  // Read all -> [GET] / item
+  app.get('/item', function (req, res){
+    res.send(lista)
+  })
 
 app.listen(3000)
